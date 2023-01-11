1ea8b869d15e15b16b2bead4aa45c1dab0d854e7
Jorge Zerpa <jorgezerpacoder@gmail.com>
2023-01-11 19:58:39 -0400

    solve details page UI bug

--- src/pages/detail/[movieId].tsx
+++ src/pages/detail/[movieId].tsx
@@ -12,19 +12,21 @@ const MovieDetail = () => {
     return (
         <>
             { isFetching ? <Loader2 /> : (
-                <div className='pt-10 px-5'>
+                <div className='pt-16 md:pt-10 px-10 sm:px-5 overflow-y-scroll max-h-screen min-h-screen'>
                     <BackButton />
                     { isSuccess && (
-                        <div className='flex mt-10'>
-                            <div className='relative min-w-[250px] w-[250px] h-[350px] bg-gray-500'>
-                                { data.results.primaryImage && <Image sizes='100%' src={data.results.primaryImage.url} alt='' fill style={{ objectFit:'cover' }} />}
+                        <div className='flex mt-10 flex-col md:flex-row pb-10'>
+                            <div className='w-full flex justify-center md:block md:w-auto '>
+                                <div className='relative min-w-[250px] w-[250px] h-[350px] bg-gray-500 shadow-black shadow-lg'>
+                                    { data.results.primaryImage && <Image sizes='100%' src={data.results.primaryImage.url} alt='' fill style={{ objectFit:'cover' }} />}
+                                </div>
                             </div>
                             <div className='px-3'>
                                 <p className='font-bold text-2xl'>{data.results.titleText?.text}</p>
                                 <p className='font-thin mb-5'>{data.results.releaseYear?.year}</p>
                                 <p className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores aut nulla reprehenderit modi quam iusto vero, ratione sequi, ab optio rerum quis cum. Laborum perferendis, aut ad incidunt quisquam optio.</p>
                                 <div className='flex mt-5 w-full'>
-                                    <button className='px-5 py-2 font-bold text-white bg-green-700 rounded-xl'>Buy</button>
+                                    <button className='px-5 py-2 font-bold text-white bg-green-700 rounded-xl shadow-lg shadow-black'>Watch</button>
                                 </div>
                             </div>
                         </div>
